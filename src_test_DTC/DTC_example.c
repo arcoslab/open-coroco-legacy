411199468e4749ecb67d85d35247ccd4109cffa7
Sebastian Chinchilla Gutierrez <tumacher@gmail.com>
2013-08-11 22:43:24 -0600

    Flux-linkage calculation added with sector calculation.

--- src_test_DTC/DTC_example.c
+++ src_test_DTC/DTC_example.c
@@ -88,6 +88,14 @@ float V_sQ=0.0f;
 float V_s =0.0f;
 float cita_V_s=0.0f;
 
+float psi_sD=0.0f;
+float psi_sQ=0.0f;
+float psi_s=0.0f;
+float psi_alpha=0.0f;
+
+float R_s=0.0f;
+
+
 switching_states (&S_A,&S_B,&S_C);
 V_sD=direct_stator_voltage_V_sD     (S_A,S_B,S_C,U_d);
 V_sQ=quadrature_stator_voltage_V_SQ (S_B,S_C,U_d);
@@ -98,6 +106,11 @@ i_sD=direct_stator_current_i_sD     (i_sA);
 i_sQ=quadrature_stator_current_i_sQ (i_sA,i_sB);
 i_s =vector_magnitude(i_sQ,i_sD);
 cita_i_s=vector_angle(i_sQ,i_sD);
+
+psi_sD   =direct_stator_flux_linkage_estimator_psi_sD     (TICK_PERIOD,V_sD,i_sD,R_s);
+psi_sQ   =quadrature_stator_flux_linkage_estimator_psi_sQ (TICK_PERIOD,V_sQ,i_sQ,R_s);
+psi_s    =stator_flux_linkage_magnite_psi_s               (psi_sD,psi_sQ);
+psi_alpha=stator_flux_linkage_sector_alpha                (psi_sD,psi_sQ);
 
 
 //if(S_A!=2 || S_B!=2 || S_C!=2)
