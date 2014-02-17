2dcf02e6dac8308e830ea585e52bf26e9e128578
tumacher <tumacher@Veda.Veda>
2014-02-17 16:21:54 -0600

    Testing: wcutoff=100 load_angle=15°

--- src_DTC-SVM/Python/Serial_Stm32f4.py
+++ src_DTC-SVM/Python/Serial_Stm32f4.py
@@ -50,7 +50,7 @@ class Serial_Stm32f4(object):
         self.new_data_line      = ''
         self.read_capture_state = 'not_collecting'
         self.tag_comment        = ''
-        self.aditional_comment=' ntm_motor, torque controller, psi_ref=Psi_F=0.0014, Ud=70%,actual Ud and currents, wcutoff=100, load angle=5deg'
+        self.aditional_comment=' multirotor_motor, torque controller, psi_ref=Psi_F=0.0016, Ud=70%,actual Ud and currents, wcutoff=100, load angle=15deg'
         self.driving_counter    = 0
         self.various_counter     = 0
         self.type_of_test       = 0        
