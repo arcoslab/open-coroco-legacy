bb4af67b610b4be43ee41a18152a5a2ef93aa4de
lenapizarro <lena.pizarro@outlook.com>
2015-03-13 16:01:04 -0600

    script.py: xfce4-terminal dimension and title edited.

--- baldor_control_utils/Subprocesses/script.py
+++ baldor_control_utils/Subprocesses/script.py
@@ -1,15 +1,32 @@
 import os
 import subprocess
 
-joystick = 'xfce4-terminal --working-directory=/home/lenouch/local/src/open-coroco/baldor_control_utils/Subprocesses/ -e "python ps3controller.py" -H '
-test_1 = 'xfce4-terminal --working-directory=/home/lenouch/local/src/open-coroco/baldor_control_utils/Subprocesses/ -e "python test_1.py " -H '
-test_2 = 'xfce4-terminal --working-directory=/home/lenouch/local/src/open-coroco/baldor_control_utils/Subprocesses/ -e "python test_2.py " -H '
-test_3 = 'xfce4-terminal --working-directory=/home/lenouch/local/src/open-coroco/baldor_control_utils/Subprocesses/ -e "python test_3.py " -H '
-test_4 = 'xfce4-terminal --working-directory=/home/lenouch/local/src/open-coroco/baldor_control_utils/Subprocesses/ -e "python test_4.py " -H '
+#Change directory. 
+directory = '/home/lenouch/local/src/open-coroco/baldor_control_utils/Subprocesses/'
+try: 
+	yarpserver = 'xfce4-terminal --working-directory='+directory+' -e "yarpserver --write" --title=Yarpserver --geometry 40x20+0-0 '
+	joystick = 'xfce4-terminal --working-directory='+directory+' -e "python ps3controller.py" --title=Joystick --geometry 40x20-0+0 '
+	test_1 = 'xfce4-terminal --working-directory='+directory+' -e "python test_1.py " --title=Test1 --geometry 40x20-0-0 '
+	test_2 = 'xfce4-terminal --working-directory='+directory+' -e "python test_2.py " --title=Test2 --geometry 40x20+0+0 '
+	test_3 = 'xfce4-terminal --working-directory='+directory+' -e "python test_3.py " --title=Test3 --geometry 40x20-300-100'
+	test_4 = 'xfce4-terminal --working-directory='+directory+' -e "python test_4.py " --title=Test4 --geometry 40x20+300+100 '
+
+	os.system(yarpserver)	
+	os.system(joystick)
+	os.system(test_1)
+	os.system(test_2)
+	os.system(test_3)
+	os.system(test_4)
 
 
-os.system(joystick)
-os.system(test_1)
-os.system(test_2)
-os.system(test_3)
-os.system(test_4)
+	while(True): 
+		prog = input('Program running ')
+
+except KeyboardInterrupt:
+	
+	print ("Killing all ")
+	kill = "killall python yarpserver"
+	os.system(kill)
+	
+
+
