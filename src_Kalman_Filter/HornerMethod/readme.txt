d139ecae4d87d8555410bf3c7cdd0fd95cf70f54
jechavarriah <negrotico19@gmail.com>
2015-03-19 14:00:40 -0600

    FixedPoint_Proyect: This commit aims to update de Resultados_de_Validacion folder with the historial of all the work in the moment

---
+++ src_Kalman_Filter/HornerMethod/readme.txt
@@ -0,0 +1,3 @@
+El algoritmo de Horner se usa a menudo para convertir entre distintos sistemas numéricos posicionales — en cuyo caso x es la base del sistema numérico, y los coeficientes ai son los dígitos de la representación del número dado en la base x — y puede usarse también si x es una matriz, en cuyo caso la carga computacional se reduce aún más.
+
+http://en.wikipedia.org/wiki/Horner%27s_method
---
+++ src_Kalman_Filter/sources/src_DTC-SVM/Print/Print.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2013 ARCOS-Lab Universidad de Costa Rica
+ * Author: Federico Ruiz Ugalde <memeruiz@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+void clean_stdin(void);
+void stdin_init(void);
+bool receive_a_string(char*);
+
